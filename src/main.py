1443e54d58f94d8b59aa4d5fa62f3dac6121fba8
Сергей Рубцов <climalt@yandex.ru>
2025-07-14 22:00:41 +0300

    Add routing, mock /users/me endpoint

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
 from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 
 FRONTEND_DIR = Path(__file__).parent / "frontend"
 STATIC_FILES_DIR = Path(__file__).parent / "static"
 
 app = FastAPI()
+api_router = APIRouter(prefix="/frontend-api")
+
+
+@api_router.get("/users/me")
+def mock_get_user():
+    mock_user_data = {
+        "profileId": 0,
+        "email": "user@example.com",
+        "username": "JohnDoe",
+        "registeredAt": "string",
+        "updatedAt": "string",
+        "isActive": True,
+    }
+    return JSONResponse(content=mock_user_data, status_code=200)
+
+
+app.include_router(api_router)
+
 
 app.mount(
     "/static",
