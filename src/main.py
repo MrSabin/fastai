43030a10cba890787020e6b2b4fe86dd9366eb77
Сергей Рубцов <climalt@yandex.ru>
2025-07-16 21:17:35 +0300

    Add Pydantic models, add error response

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from fastapi import APIRouter, FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel, EmailStr, Field
 
 FRONTEND_DIR = Path(__file__).parent / "frontend"
 STATIC_FILES_DIR = Path(__file__).parent / "static"
@@ -11,7 +12,26 @@ app = FastAPI()
 api_router = APIRouter(prefix="/frontend-api")
 
 
-@api_router.get("/users/me")
+class ErrorModel(BaseModel):
+    detail: str
+
+
+class UserDetailsResponse(BaseModel):
+    profileId: int = Field(description="User profile ID")
+    email: EmailStr = Field(description="User email")
+    username: str = Field(description="Username", max_length=255)
+    registeredAt: str = Field(description="User registration date")
+    updatedAt: str = Field(description="Last profile update")
+    isActive: bool = Field(description="Is active user")
+
+
+@api_router.get(
+    "/users/me",
+    response_model=UserDetailsResponse,
+    responses={
+        200: {"description": "User details"},
+        401: {"model": ErrorModel, "description": "User unauthorized"},
+    })
 def mock_get_user():
     mock_user_data = {
         "profileId": 0,
