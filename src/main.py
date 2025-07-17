1533c8db3b90aef3af852ab7f18f54034199a4d4
Сергей Рубцов <climalt@yandex.ru>
2025-07-17 21:31:01 +0300

    Minor edits after review

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 from fastapi import APIRouter, FastAPI
-from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, EmailStr, Field
 
@@ -12,17 +11,23 @@ app = FastAPI()
 api_router = APIRouter(prefix="/frontend-api")
 
 
-class ErrorModel(BaseModel):
+class ErrorResponse(BaseModel):
     detail: str
 
 
 class UserDetailsResponse(BaseModel):
-    profileId: int = Field(description="User profile ID")
-    email: EmailStr = Field(description="User email")
-    username: str = Field(description="Username", max_length=255)
-    registeredAt: str = Field(description="User registration date")
-    updatedAt: str = Field(description="Last profile update")
-    isActive: bool = Field(description="Is active user")
+    profileId: int
+    """User profile ID"""
+    email: EmailStr
+    """User email"""
+    username: str = Field(max_length=255)
+    """Username"""
+    registeredAt: str
+    """User registration date"""
+    updatedAt: str
+    """Last profile update"""
+    isActive: bool
+    """Is active user"""
 
 
 @api_router.get(
@@ -30,10 +35,10 @@ class UserDetailsResponse(BaseModel):
     response_model=UserDetailsResponse,
     responses={
         200: {"description": "User details"},
-        401: {"model": ErrorModel, "description": "User unauthorized"},
+        401: {"model": ErrorResponse, "description": "User unauthorized"},
     })
 def mock_get_user():
-    mock_user_data = {
+    return {
         "profileId": 0,
         "email": "user@example.com",
         "username": "JohnDoe",
@@ -41,7 +46,6 @@ def mock_get_user():
         "updatedAt": "string",
         "isActive": True,
     }
-    return JSONResponse(content=mock_user_data, status_code=200)
 
 
 app.include_router(api_router)
