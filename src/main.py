9f6d48f490a31e401524dcb39f110e2509223238
Сергей Рубцов <climalt@yandex.ru>
2025-08-07 19:12:14 +0300

    Remove static dir mount, create app factory function

--- src/main.py
+++ src/main.py
@@ -3,27 +3,25 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from routers.sites import router as sites_router
-from routers.users import router as users_router
+from src.config import Settings
+from src.routers.sites import router as sites_router
+from src.routers.users import router as users_router
 
 FRONTEND_DIR = Path(__file__).parent / "frontend"
-STATIC_FILES_DIR = Path(__file__).parent / "static"
-
-app = FastAPI()
 
 
-app.include_router(users_router, prefix="/frontend-api")
-app.include_router(sites_router, prefix="/frontend-api")
+def create_app():
+    app = FastAPI()
 
+    app.include_router(users_router, prefix="/frontend-api")
+    app.include_router(sites_router, prefix="/frontend-api")
 
-app.mount(
-    "/static",
-    StaticFiles(directory=STATIC_FILES_DIR),
-    name="static-files",
-)
+    app.mount(
+        "/",
+        StaticFiles(directory=FRONTEND_DIR, html=True),
+        name="frontend",
+    )
 
-app.mount(
-    "/",
-    StaticFiles(directory=FRONTEND_DIR, html=True),
-    name="frontend",
-)
+    app.state.settings = Settings()
+
+    return app
